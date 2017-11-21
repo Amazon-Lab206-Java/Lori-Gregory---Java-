f319d84fd8315cb2d484059ef636197095b4901b
Lori Gregory <32581719+LoriGregory@users.noreply.github.com>
2017-11-20 21:06:53 -0700

    Delete StringDemo.java

--- StringDemo.java
+++
@@ -1,7 +0,0 @@
-public class StringDemo {
-     public static void main(String[] args) {
-        String ninja = "Coding Dojo is Awesome!";
-        int length = ninja.length();
-        System.out.println( "String Length is : " + length );
-     }
-}
